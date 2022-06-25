d719471acb34e172fbd93a02e79b8004f29ffda0
Nusaybah607 <105378100+Nusaybah607@users.noreply.github.com>
2022-06-25 15:21:21 +0100

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,26 @@
+function addition(firstNumber, secondNumber){
+    const sum = firstNumber + secondNumber
+    console.log(`The sum of ${firstNumber} and ${secondNumber} is ${sum}`)
+}
+
+function subtraction(firstNumber, secondNumber){
+    const difference = firstNumber - secondNumber
+    console.log(`The difference between ${firstNumber} and ${secondNumber} is ${difference}`);
+}
+
+function multiplication(firstNumber, secondNumber){
+    const multiple = firstNumber * secondNumber
+    console.log(`The multiplication of ${firstNumber} and ${secondNumber} is ${multiple}`);
+}
+
+
+function division(firstNumber, secondNumber){
+    const division = firstNumber/ secondNumber 
+    console.log(`The division of ${firstNumber} by ${secondNumber} is ${division}`);
+}
+    
+
+addition(3,4)
+subtraction(10,6)
+multiplication(10,6)
+division(10,2)
